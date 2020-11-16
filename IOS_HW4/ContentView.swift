3ea3b77f9d3c219269777e050f882dc7f9a644ce
Eric's MB <eric17618@gmail.com>
2020-11-16 15:06:25 +0800

    Initial Commit

---
+++ IOS_HW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IOS_HW4
+//
+//  Created by 翁星宇 on 2020/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IOS_HW4/IOS_HW4App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_HW4App.swift
+//  IOS_HW4
+//
+//  Created by 翁星宇 on 2020/11/16.
+//
+
+import SwiftUI
+
+@main
+struct IOS_HW4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
